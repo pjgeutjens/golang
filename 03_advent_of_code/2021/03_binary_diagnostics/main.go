d02f14094f8e886d6c302dbd75dcd78fcde00287
Pieter Jan Geutjens <pieter.geutjens@leapconsulting.be>
2023-09-16 22:48:20 +0200

    adventofcode d3p2 work in progress..

--- 03_advent_of_code/2021/03_binary_diagnostics/main.go
+++ 03_advent_of_code/2021/03_binary_diagnostics/main.go
@@ -37,10 +37,52 @@ func main() {
 	g, _ := strconv.ParseInt(gamma, 2, 64)
 	e, _ := strconv.ParseInt(epsilon, 2, 64)
 	fmt.Print(g * e)
+
+	fmt.Println(oxygen_generator_rating(readings))
 }
 
-func oxygen_generator_rating(readings []string, position int) string {
-	return "todo"
+func get_majority_bit(readings []string) ([]byte, error) {
+	if len(readings) == 0 {
+		return []byte{}, fmt.Errorf("empty readings")
+	}
+
+	reading_length := len(readings[0])
+	var tally []int = make([]int, reading_length)
+	for i := 0; i < len(readings); i++ {
+		for j := 0; j < reading_length; j++ {
+			if readings[i][j] == '1' {
+				tally[j]++
+			}
+		}
+	}
+	result := make([]byte, reading_length)
+	for i := range tally {
+		if tally[i] < len(readings)/2 {
+			result[i] = '0'
+		} else {
+			result[i] = '1'
+		}
+	}
+	return result, nil
+}
+
+func oxygen_generator_rating(readings []string) string {
+	reading_length := len(readings[0])
+	for len(readings) > 1 {
+		for j := 0; j < reading_length; j++ {
+			for i := 0; i < len(readings); i++ {
+				keep, err := get_majority_bit(readings)
+				if err != nil {
+					return ""
+				}
+				if readings[i][j] != keep[j] {
+					readings[i] = readings[len(readings)-1]
+					readings = readings[:len(readings)-1]
+				}
+			}
+		}
+	}
+	return readings[0]
 }
 
 func read_from_file(filepath string) ([]string, error) {
